66da6ec7ec8b7c03d6d4843cf0638185d96e7453
AlexeyCheremnykh <chmnkh@gmail.com>
2017-12-06 21:45:53 +0700

    Шапка есть (без js)

---
+++ src/entry.js
@@ -0,0 +1,5 @@
+import "./fonts/Lato/lato.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+import "jquery";
+import "./blocks/header/header.styl";
+import "./blocks/search/search.styl";
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/entry.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.styl$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.png$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'img/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'fonts/[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.pug$/,
+                use: ['html-loader', 'pug-html-loader']
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery",
+            "window.$": "jquery"
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/blocks/index.pug'
+        })
+    ]
+}      
+        

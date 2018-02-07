db94b1a82da5e2132057058b84bf17c7b0b68af7
AlexeyCheremnykh <chmnkh@gmail.com>
2018-02-07 23:11:23 +0700

    articles: респонсив для фильтра и поиска

--- src/pages/articles/articles.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $(".articles .articles__filter-form .dropdown").select2({
-        multiple: true
-    });
-    $(".articles .articles__filter-form .dropdown").val("").change();
-})

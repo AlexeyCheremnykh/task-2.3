2db2497c877ce8489aaae1b9820194d97a381231
AlexeyCheremnykh <chmnkh@gmail.com>
2018-02-07 15:38:46 +0700

    videos: раскидал стили, элементы, написал js

--- src/pages/videos/__content/videos__content.js
+++ src/pages/videos/__content/videos__content.js
@@ -1,3 +1,18 @@
-$(document).ready(function () {
-    $(".videos__content").tabs();
-})
+class VideosContainer {
+  constructor(elem, id) {
+    this._id = id;
+    this._$container = $(elem);
+  }
+
+  initElement() {
+    this._$container.tabs();
+  }
+}
+
+const $videosContainer = $('.videos__content');
+let id = 0;
+$videosContainer.each((index, elem) => {
+  const videosContainer = new VideosContainer(elem, id);
+  videosContainer.initElement();
+  id += 1;
+});

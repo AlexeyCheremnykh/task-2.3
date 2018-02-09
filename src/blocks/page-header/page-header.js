cfbf81dc5d01074fdf0bc4b59e9901f2a4f367cb
AlexeyCheremnykh <chmnkh@gmail.com>
2018-02-10 01:01:25 +0700

    page-header: дописал стили, js, сменил пикчу на jpg

--- src/blocks/page-header/page-header.js
+++ src/blocks/page-header/page-header.js
@@ -1,21 +1,48 @@
-$(document).ready(function() {
-    // menu    
-    $(".page-header__menu").click(function() {
-        if ($(".page-header__nav").hasClass("page-header__nav_default")) {
-            $(".page-header__nav").removeClass("page-header__nav_default").addClass("page-header__nav_open");
-        } else {
-            $(".page-header__nav").removeClass("page-header__nav_open").addClass("page-header__nav_default");
-        }
-    });
+class PageHeader {
+  constructor(elem, id) {
+    this._id = id;
+    this._$header = $(elem);
+    this._$menuIcon = $(elem).find('.js-page-header__menu-icon');
+    this._$nav = $(elem).find('.js-page-header__nav');
+  }
 
-    var mql = window.matchMedia("(min-width: 765px)");
-    var handleMediaChange = function (mediaQueryList) {
-        if (mediaQueryList.matches) {
-            if ($(".page-header__nav").hasClass("page-header__nav_open")) {
-                $(".page-header__nav").removeClass("page-header__nav_open").addClass("page-header__nav_default");
-            }
-        }
-    }
-    mql.addListener(handleMediaChange);
-    handleMediaChange(mql);
-})
+  initEventListeners() {
+    const self = this;
+
+    const hideNav = function hideSiteNavigationInMenu(mediaQuery) {
+      if (mediaQuery.matches) {
+        self._$nav.addClass('page-header__nav_closed');
+      }
+    };
+
+    const showNav = function showSiteNavigation(mediaQuery) {
+      if (mediaQuery.matches) {
+        self._$nav.removeClass('page-header__nav_closed page-header__nav_open');
+      }
+    };
+
+    const hideNavMedia = window.matchMedia('(max-width: 765px)');
+    hideNavMedia.addListener(hideNav);
+
+    const showNavMedia = window.matchMedia('(min-width: 765px)');
+    showNavMedia.addListener(showNav);
+
+    const toggleNav = function showOrHideSiteNavigation() {
+      if (self._$nav.hasClass('page-header__nav_closed')) {
+        self._$nav.removeClass('page-header__nav_closed').addClass('page-header__nav_open');
+      } else {
+        self._$nav.removeClass('page-header__nav_open').addClass('page-header__nav_closed');
+      }
+    };
+
+    this._$menuIcon.on('click', toggleNav);
+  }
+}
+
+const $header = $('.js-page-header');
+let id = 0;
+$header.each((index, elem) => {
+  const header = new PageHeader(elem, id);
+  header.initEventListeners();
+  id += 1;
+});

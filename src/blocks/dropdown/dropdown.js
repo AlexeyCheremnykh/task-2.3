87d30947d3e4121e2bdac47e4006ff9696b0ad56
AlexeyCheremnykh <chmnkh@gmail.com>
2018-03-05 22:25:06 +0700

    Заменил обновленные со 2 макета блоки

--- src/blocks/dropdown/dropdown.js
+++ src/blocks/dropdown/dropdown.js
@@ -1,19 +1,26 @@
 class Dropdown {
-  constructor(elem, id) {
+  constructor(selectElem, id) {
     this._id = id;
-    this._$dropdown = $(elem);
+    this._$dropdown = $(selectElem).parent();
+    this._$dropdownSelect = $(selectElem);
   }
 
   initElement() {
     if (this._$dropdown.hasClass('js-dropdown_multiple')) {
-      this._$dropdown.select2({
+      this._$dropdownSelect.select2({
         multiple: true,
+        dropdownParent: this._$dropdown,
+        containerCssClass: 'dropdown__container',
+        dropdownCssClass: 'dropdown__options',
       });
-      this._$dropdown.val('').change();
+      this._$dropdownSelect.val('').change();
     } else {
-      this._$dropdown.select2({
+      this._$dropdownSelect.select2({
         placeholder: 'Choose An Option',
         minimumResultsForSearch: -1,
+        dropdownParent: this._$dropdown,
+        containerCssClass: 'dropdown__container',
+        dropdownCssClass: 'dropdown__options',
       });
     }
 
@@ -21,9 +28,9 @@ class Dropdown {
   }
 }
 
-const $dropdown = $('.js-dropdown');
+const $dropdownSelect = $('.js-dropdown__select');
 let id = 0;
-$dropdown.each((index, elem) => {
+$dropdownSelect.each((index, elem) => {
   const dropdown = new Dropdown(elem, id);
   dropdown.initElement();
   id += 1;

fa89c5d2246933c623b1910b1952adfd8e3dea23
AlexeyCheremnykh <chmnkh@gmail.com>
2018-03-16 16:52:42 +0700

    Пересобрал

--- bundle.js
+++ bundle.js
@@ -19092,7 +19092,7 @@ module.exports = __webpack_require__.p + "img/user-profile-man.jpg";
   }
 
   initElement() {
-    if (this._$slider.hasClass('slider_type_red')) {
+    if (this._$slider.hasClass('slider_type_not-labeled')) {
       this._$sliderLine
         .slider({
           min: this._$slider.data('min'),
@@ -19230,12 +19230,16 @@ class Percentage {
     const percentageValue = parseInt(this._$value.text(), 10) / 100;
     const percentageSize = parseInt(this._$percentage.css('height'), 10);
     const percentageThickness = percentageSize / constants.SIZE_THICKNESS_RATIO;
-
+    const getFillColor = () => {
+      if (this._$percentage.hasClass('percentage_color_red')) return '#e75735';
+      if (this._$percentage.hasClass('percentage_color_blue')) return '#4e6ea3';
+      return '#000000';
+    };
     this._$percentage.circleProgress({
       canvas: this._$circleProgress[0],
       value: percentageValue,
       size: percentageSize,
-      fill: '#e75735',
+      fill: getFillColor(),
       thickness: percentageThickness,
       emptyFill: '#ffffff',
       startAngle: -Math.PI / 2,
